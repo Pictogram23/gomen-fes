82016709569bc43bbea020fc5e3dc13f2c5185cb
Pictogram23 <shanchengyinger@gmail.com>
2024-05-08 07:00:59 +0900

    検索機能の実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import fs from 'fs'
+'use client'
+import { TextField } from '@mui/material'
 import Grid from '@mui/material/Grid'
+import { useState } from 'react'
 import PosterCard from './_components/PosterCard'
 
 type Poster = {
@@ -9,18 +11,26 @@ type Poster = {
 }
 
 export default function Home() {
-  const posterlist: Poster[] = JSON.parse(fs.readFileSync('./app/posterlist.json', 'utf8'))
-
+  const [keyword, setKeyword] = useState<string>('')
+  const posterlist: Poster[] = require('./posterlist.json')
   return (
     <main>
       <h1>素材を提供してくれた素敵な人たち</h1>
+      <TextField
+        sx={{ mb: 5 }}
+        label='Search'
+        variant='outlined'
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <Grid container spacing={2} justifyContent='center'>
         {posterlist.map((poster) => {
-          return (
-            <>
-              <PosterCard name={poster.name} url={poster.url} comment={poster.comment} />
-            </>
-          )
+          if (poster.name.indexOf(keyword) != -1) {
+            return (
+              <>
+                <PosterCard name={poster.name} url={poster.url} comment={poster.comment} />
+              </>
+            )
+          }
         })}
       </Grid>
     </main>
