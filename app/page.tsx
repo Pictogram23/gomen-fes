897aee3ad631a68fbc470f45d9461fce21e6055e
Pictogram23 <shanchengyinger@gmail.com>
2024-05-08 01:26:35 +0900

    ネタ提供者一覧を簡単なもので実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
+import fs from 'fs'
 import Grid from '@mui/material/Grid'
 import PosterCard from './_components/PosterCard'
 
+type Poster = {
+  name: string
+  url: string
+  comment: string
+}
+
 export default function Home() {
+  const posterlist: Poster[] = JSON.parse(fs.readFileSync('./app/posterlist.json', 'utf8'))
+
   return (
     <main>
       <h1>素材を提供してくれた素敵な人たち</h1>
       <Grid container spacing={2} justifyContent='center'>
-        <PosterCard name='hoge' url='hoge' comment='hoge' />
+        {posterlist.map((poster) => {
+          return (
+            <>
+              <PosterCard name={poster.name} url={poster.url} comment={poster.comment} />
+            </>
+          )
+        })}
       </Grid>
     </main>
   )
