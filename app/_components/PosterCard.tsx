b856f8781cde6d31bb9ee625e2fd147ff663f581
Pictogram23 <shanchengyinger@gmail.com>
2024-06-01 08:17:15 +0900

    詳細モーダルの実装

--- app/_components/PosterCard.tsx
+++ app/_components/PosterCard.tsx
@@ -1,4 +1,16 @@
-import { Avatar, Grid, Link } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import {
+  Avatar,
+  Button,
+  ButtonBase,
+  Grid,
+  Link,
+  Modal,
+  TableBody,
+  TableCell,
+  TableRow,
+  createTheme,
+} from '@mui/material'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
@@ -8,34 +20,141 @@ import * as React from 'react'
 interface PosterCardProps {
   name: string
   twitter_id: string
+  roll: string
+  exception: string
+  others: string
+  credit: string
+  earning: string
+  original: string
+  comment: string
 }
 
-const PosterCard = ({ name, twitter_id }: PosterCardProps) => {
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 768,
+      lg: 1025,
+      xl: 1536,
+    },
+  },
+})
+
+const PosterCard = ({
+  name,
+  twitter_id,
+  roll,
+  exception,
+  others,
+  credit,
+  earning,
+  original,
+  comment,
+}: PosterCardProps) => {
+  const [open, setOpen] = React.useState<boolean>(false)
+  const row_key_list = [
+    '担当',
+    'パロディ禁止曲',
+    'ニコニコ以外への投稿',
+    'クレジットの記載',
+    '収益化',
+    'オリジナルライセンス',
+    'コメント',
+  ]
+  const row_content_list = [roll, exception, others, credit, earning, original, comment]
   const card = (
     <React.Fragment>
-      <CardContent>
-        <Grid container justifyContent='center'>
-          <Avatar
-            src={'/icons/' + twitter_id + '.jpg'}
-            variant='square'
-            sx={{ minWidth: 75, minHeight: 75 }}
-          />
-          <Box display='flex' alignItems='center'>
-            <Box minWidth={200}>
-              <Typography variant='body1' mt={0.5}>
-                {name}
-              </Typography>
-              <Link href={'https://x.com/' + twitter_id} sx={{ mb: 1.5 }} color={'text.secondary'}>
-                {twitter_id}
-              </Link>
+      <ButtonBase onClick={() => setOpen(true)}>
+        <CardContent>
+          <Grid container justifyContent='center'>
+            <Avatar
+              src={'/icons/' + twitter_id + '.jpg'}
+              variant='square'
+              sx={{ minWidth: 75, minHeight: 75 }}
+            />
+            <Box display='flex' alignItems='center'>
+              <Box minWidth={200}>
+                <Typography variant='body1' mt={0.5}>
+                  {name}
+                </Typography>
+                <Link
+                  /*href={'https://x.com/' + twitter_id}*/ sx={{ mb: 1.5 }}
+                  color={'text.secondary'}
+                >
+                  {twitter_id}
+                </Link>
+              </Box>
             </Box>
-          </Box>
-        </Grid>
-      </CardContent>
+          </Grid>
+        </CardContent>
+      </ButtonBase>
     </React.Fragment>
   )
   return (
     <>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box
+          sx={{
+            position: 'absolute' as 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '80vw',
+            height: '80vh',
+            overflowY: 'scroll',
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Box sx={{ textAlign: 'right' }}>
+            <ButtonBase onClick={() => setOpen(false)}>
+              <CloseIcon sx={{ width: 40, height: 40 }} />
+            </ButtonBase>
+          </Box>
+
+          <Box sx={{ textAlign: 'center' }}>
+            <Grid container justifyContent='center'>
+              <Avatar
+                src={'/icons/' + twitter_id + '.jpg'}
+                variant='square'
+                sx={{ minWidth: 150, minHeight: 150, mr: { xs: 0, sm: 5 }, mb: { xs: 3, sm: 0 } }}
+              />
+              <Box display='flex' alignItems='center'>
+                <Box>
+                  <Typography variant='h3'>{name}</Typography>
+                  <Link
+                    variant='h6'
+                    href={'https://x.com/' + twitter_id}
+                    sx={{ mb: 1.5 }}
+                    color={'text.secondary'}
+                  >
+                    {twitter_id}
+                  </Link>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid container justifyContent='center' mt={5}>
+              <Box width={{ xs: '100%', sm: '75%', lg: '50%' }} display='flex' alignItems='center'>
+                <TableBody>
+                  {row_key_list.map((value, index) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell width='60%' component='th' scope='row'>
+                          {value}
+                        </TableCell>
+                        <TableCell align='center'>{row_content_list[index]}</TableCell>
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Box>
+            </Grid>
+          </Box>
+        </Box>
+      </Modal>
       <Box sx={{ minWidth: 275, padding: 1 }}>
         <Card variant='outlined'>{card}</Card>
       </Box>
